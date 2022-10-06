07201104c887246071ced51a5f8bab426c10388b
Munish Kumar <77374065+munish27@users.noreply.github.com>
2022-10-06 12:59:37 +0530

    updated commit

---
+++ file.js
@@ -0,0 +1,11 @@
+
+// Program to take the input from the user and print it.
+var count = 0;
+while(count<5){
+    var value = prompt("Enter the value: ");
+    document.write(value);
+    document.getElementById("pro2") = value;
+    count++;
+}
+
+
---
+++ file1.js
@@ -0,0 +1,10 @@
+// Program to get total marks
+var total = 0;
+var count = 0;
+while(count<5){
+    var marks = prompt("Enter the marks: ");
+    total += parseInt(marks);
+    count++;
+}
+document.write(total);
+document.getElementById("pro2") = total;
