d10a404debfd10c4e75a8cc97479371fe8cf9262
Munish Kumar <itsdark099@gmail.com>
2022-10-06 12:45:01 +0530

    Initial commit

---
+++ file.js
@@ -0,0 +1,19 @@
+
+// Program to take the nput from the user and print it.
+var count = 0;
+while(count<5){
+    var value = prompt("Enter the value: ");
+    document.write(value);
+    count++;
+}
+
+
+// Program to get total marks
+var total = 0;
+var count = 0;
+while(count<5){
+    var marks = prompt("Enter the marks: ");
+    total += parseInt(marks);
+    count++;
+}
+document.write(total);
